2b3ef603b0c1127288786882f2736bcea06fb99b
cmh <charles.m.hall@gmail.com>
2023-09-14 00:28:58 -0400

    reorg tests

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-for t in `cat tests`; do
+m=$1
+e=$2
+
+for t in `cat tests.$m$e`; do
   echo -n "$t: "
-  ../pg $t printstates &> $t.out
-  diff -u $t.res $t.out &> $t.diff
+  ../pg $t printstates $m $e &> $t.$m$e.out
+  diff -u $t.$m$e.res $t.$m$e.out &> $t.$m$e.diff
   if [ $? -ne 0 ]; then
     echo -e "fail *****"
   else
     echo -e "pass"
-    rm -f $t.out $t.diff
+    rm -f $t.$m$e.out $t.$m$e.diff
   fi
 done
 
